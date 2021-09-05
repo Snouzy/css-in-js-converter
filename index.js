7866f8591b5ea5caf04466424a7d6dc1370cd1de
Mathias <mathiasnouzy@gmail.com>
2021-09-05 19:43:15 +0200

    refactor: func name and console.log remove

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import alfy from 'alfy'
+<<<<<<< Updated upstream
 
 alfy.output([
 	{
 		title: alfy.input
+=======
+import { removeMaj } from "./helpers.js";
+
+alfy.output([
+	{
+    uid: "css-in-js-to-css",
+    title: "From CSS-in-JS to regular CSS",
+    subtitle: "Transform CSSinJS to regular CSS",
+    autocomplete: "From CSS-in-JS to regular CSS",
+    text: {
+      "copy": cssInJSToRegular(alfy.input),
+      "largetype": cssInJSToRegular(alfy.input)
+    }
+>>>>>>> Stashed changes
 	},
 	{
 		title: 'Rainbow'
